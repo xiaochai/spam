1833fa40fc88784fb9c249ac6dbccd26670112a5
liqingshou <liqingshou@bytedance.com>
2024-10-10 15:52:41 +0800

    flight radar spam

---
+++ flight_radar_24/src/bin/get_kml.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn login() {
+
+}
+
+fn load_application_config(){
+
+}
+
+pub struct AccountConf{
+    pub user_name:String,
+    pub password :String,
+}
+#[]
+pub struct ApplicationConfig{
+    pub account: AccountConf
+}
